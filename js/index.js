bc036592b4a1eb13215192a0caf7bab510e1e4e9
Otávio Barreto de Oliveira Silva <otaviosilva2632@gmail.com>
2020-04-10 21:53:36 -0300

    Removido menu do bootstrap e adicionado menu lateral

--- js/index.js
+++ js/index.js
@@ -1,11 +1,3 @@
 $(document).ready(() => {
-  $(window).scroll(() => {
-    window.pageYOffset >= 20 ? 
-      $('.navbar').addClass('bg-dark').removeClass("scrollFalse") : 
-      $('.navbar').removeClass('bg-dark').addClass("scrollFalse")
-  })
-
-  window.width > '991px' ? 
-    $('.navbar').removeClass('bg-dark') :
-    $('.navbar').addClass('bg-dark')
+  
 })
