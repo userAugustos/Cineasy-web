6e873021c5068eaebd32d55bfe79a26abff3b046
userAugustos <felipe_augustos84@outlook.com.br>
2020-04-17 17:23:23 -0300

    Ciração cards de filmes e planos & modal divulgativo

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 $(document).ready(() => {
   
 })
+
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 1400 && $("#myModal").attr("displayed") === "false") {
+    $('#myModal').modal('show');
+    $("#myModal").attr("displayed", "true");
+    }
+});
